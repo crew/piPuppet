292fe938768cd57a0344ff42ec1bacf1a4117420
root <root@raspberrypi>
2014-04-10 01:34:11 +0000

    script to install and initialy configure the pies for puppet

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
-curl -O http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.6.18-1puppetlabs1_all.deb
-curl -O curl -O https://gist.github.com/carlossg/8578202/raw/70689b1b74517cc4b0743e54d84dae8375503159/videbcontrol.sh
-bash videbcontrol.sh facter_1.6.18-1puppetlabs1_all.deb
+sudo dpkg -i puppetlabs-release-wheezy.deb
+sudo apt-get update -y
 # remove dependency to dmidecode in the editor that opens
 sudo dpkg -i facter_1.6.18-1puppetlabs1_all.modified.deb
 # If you want to use ruby 1.9 instead of 1.8
-sudo apt-get install libaugeas-ruby1.9.1 ruby1.9.1
- 
+sudo apt-get install libaugeas-ruby1.9.1 ruby1.9 -y
+sudo apt-get -f install -y
 # puppet may not install if all the dependencies are not listed
-sudo apt-get install puppet puppet-common
+sudo apt-get install puppet puppet-common -y
 # mark dependencies as automatically installed so they are removed when removing puppet
 sudo apt-mark auto facter libaugeas-ruby1.9.1 puppet-common
-
+mv -f files/puppet.conf /etc/puppet/puppet.conf
+mv -f files/puppet /etc/default/puppet
